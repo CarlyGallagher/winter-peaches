cc2328f8454f3fefc80261810340f7889d9f69db
CarlyGallagher <jagllaghercarly3@yahoo.com>
2023-02-05 20:05:54 -0800

    created function for longitude and latitude

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,21 @@
 var APIkey = 'd3a2ff100ca39275bb1aa547304afe71'
-var queryURL = 
+var queryURL = 'http://api.openweathermap.org/data/2.5/forecast?lat=37.7749&lon=122.4194&appid=d3a2ff100ca39275bb1aa547304afe71'
+var geoCode = 'http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}'
+var search = document.getElementById('search-input');
+var searchButton = document.getElementById('search-btn');
+var city = document.getElementById('city');
 
-fetch(queryURL)
-.then((response) => response.json())
+searchButton.addEventListener('click', everythingNow);
+
+function everythingNow() {
+    var geoCode = 'http://api.openweathermap.org/geo/1.0/direct?q=' + search.value + '&limit=1&appid=' + APIkey + ''
+
+    fetch(geoCode)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data)
+        })
+
+};
