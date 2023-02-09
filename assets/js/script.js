18c931eef43c4ff09f3e469151bde0ab4d4a160d
CarlyGallagher <jagllaghercarly3@yahoo.com>
2023-02-08 19:01:24 -0800

    push through for ASK BC's, local storage btn issue

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,10 @@ var search = document.getElementById('search-input');
 var searchButton = document.getElementById('search-btn');
 var cityEl = document.getElementById('city');
 
-    searchButton.addEventListener('click', everythingNow);
+    searchButton.addEventListener('click', everythingNow)
+
+    var newBtn = $('<button>')
+
 
     function everythingNow(event) {
         event.preventDefault();
@@ -72,13 +75,48 @@ var cityEl = document.getElementById('city');
     }
 
     // sets item to local storage
+
+
+    var cityHistory = [];
+
     $('.btn').click(function () {
         var searchBtn = $(this).parent().attr('id');
+        console.log(searchBtn);
         localStorage.setItem('searchBtn', searchBtn);
-    
-        value = $(this).siblings("input").val();
-        textString = $(this).siblings("div").text();
+
+        value = $(this).siblings('input').val();
+        textString = $(this).siblings('div').text();
         localStorage.setItem(textString, JSON.stringify(value));
+
+        //get local storage and put it in cityHistory
+
+        cityHistory = localStorage.getItem('cityHistory');
+
+        //if there is Nothing in city history than
+        //cityHistory = [$(this).siblings("input").val()];
+        if(!cityHistory) {
+            cityHistory = [$(this).siblings("search-form").val()];
+        }
+
+        //else 
+        // json parse cityhistory
+        // and .push() $(this).siblings("input").val() into cityHistory
+
+        else JSON.parse('cityHistory')
+        cityHistory.push($(this).siblings("search-form").val());
+
+        //This sets everything inside of this to empty so that we can add all the citys back from 
+        // local storage. We could avoid this with another method but this is more simple.
+        $("#selectable").html("");
+
+        // This goes though the list of all citys in local and creates a button for them with the city name inside.
+        for (var i =0; i < cityHistory; i++) {
+            $("#selectable").append("<button>" + cityHistory[i] + "</button>");
+        }
+
+        //Json stringify cityHistory
+        //then set item in local storyage
+
     });
 
 $('#search-input').val(localStorage.getItem)
